581abd38a1dc4c0e78a8495618bed92d56b674f4
APPA_Programming <appa.programming@gmail.com>
2016-05-05 15:03:10 +0100

    ADD background.js for testing ADD extension.css for testing ADD jquery ADD youtubenotes.js Basic test functions about initialization + changes in DOM

---
+++ YoutubeNotes/YoutubeNotes/app/youtubenotes.js
@@ -0,0 +1,30 @@
+﻿var youtubeNotes = {
+    requestChangeDOM: function () {
+        var $label = $("<label>").text('Hello World!');
+        youtubeNotes.insertAtIndex(1, "watch-header", $label);
+    },
+    insertAtIndex: function (i, id, $elem) {
+        if (i === 0) {
+            $("#" + id).prepend($elem);
+            return;
+        }
+
+
+        $("#" + id + " > div:nth-child(" + (i) + ")").after($elem);
+    }
+};
+
+console.log('you\'r in the world of content.js');
+youtubeNotes.requestChangeDOM();
+
+//This line opens up a long-lived connection to your background page.
+var port = chrome.runtime.connect({ name: "youtubenotes" });
+port.onMessage.addListener(function (message, sender) {
+    if (message.greeting === "hello") {
+        alert(message.greeting);
+    }
+});
+
+chrome.runtime.sendMessage({ greeting: "hello" }, function (response) {
+    console.log(response.farewell);
+});
